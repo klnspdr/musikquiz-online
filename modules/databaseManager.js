9c7668bd2665c9dad8fb3566355835835a867d50
BuildTools <unconfigured@null.spigotmc.org>
2021-03-28 21:09:03 +0200

    solved error of missing file permissions for db.json by using absolute path with path.join() instead of relative path

--- modules/databaseManager.js
+++ modules/databaseManager.js
@@ -1,8 +1,9 @@
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
 const lodashId = require('lodash-id')
+const path = require('path');
 
-const adapter = new FileSync('../db.json')
+const adapter = new FileSync(path.join(__dirname, '../db.json'));
 const db = low(adapter)
 db._.mixin(lodashId)
 
